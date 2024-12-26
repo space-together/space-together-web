0f1476ad146d17a221bc484b57051119a2ab0e69
brunorwanda4 <brunorwanda4@gmail.com>
2024-12-26 04:38:01 +0200

    remove error

--- src/components/auth/authLayoutImage.tsx
+++ src/components/auth/authLayoutImage.tsx
@@ -1,7 +1,7 @@
 "use client";
 import UseTheme from "@/context/theme/use-theme";
 import { GrLinkNext } from "react-icons/gr";
-import { usePathname } from "next/navigation";
+// import { usePathname } from "next/navigation";
 import MyImage from "../my-components/myImage";
 import AuthLogo from "./auth-logo";
 import { cn } from "@/lib/utils";
@@ -9,7 +9,7 @@ import Link from "next/link";
 
 const AuthLayoutImage = () => {
   const theme = UseTheme();
-  const pathname = usePathname();
+//   const pathname = usePathname();
 
   return (
     <div className=" w-1/2 h-screen relative">
@@ -17,13 +17,13 @@ const AuthLayoutImage = () => {
         <div className={cn("p-2 rounded-full backdrop-blur-lg ")}>
           <AuthLogo />
         </div>
-        <Link className={cn("mr-4 flex items-center gap-2 rounded-full backdrop-blur-lg p-3 cupo" , theme === "dark" && "hover:bg-black/30")} href={`/`}>
+        <Link className={cn("mr-4 flex items-center gap-2 rounded-full backdrop-blur-lg p-3" , theme === "dark" && "hover:bg-black/30")} href={`/`}>
           Back to website <GrLinkNext />
         </Link>
       </div>
       <div className=" w-full h-full relative">
         <MyImage
-          src="https://img.freepik.com/free-photo/girl-studying-university-library_23-2148844686.jpg?t=st=1735179684~exp=1735183284~hmac=f0adc36ca0e306488c48df58038016f3876f5d1ce9d44274b6b1d251c3bb1b7e&w=996"
+          src="/images/7.jpg"
           alt="Picture of the author"
           className="  h-full w-full"
         />
