b9a7c8b5b3b080977c60d71e65f3180cbbb7138f
brunorwanda4 <brunorwanda4@gmail.com>
2025-01-25 13:41:39 +0200

    add new prisma

---
+++ src/services/actions/auth/register-actions.ts
@@ -0,0 +1,33 @@
+"use server";
+import { registerSchema, registerSchemaType } from "@/utils/schema/userSchema";
+
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/services/data/user";
+
+export const registerAction = async (values: registerSchemaType) => {
+  const validation = registerSchema.safeParse(values);
+  if (!validation.success) {
+    return { error: "Invalid Register Validation" };
+  }
+
+  const { name, email, password } = validation.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return {
+      error: `email already exit [${existingUser.email}], try other email`,
+  };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+  return { success: "Account created" };
+};
