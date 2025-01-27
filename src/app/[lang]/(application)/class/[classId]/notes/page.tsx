72c0b11b071ead3aa8dad7c370f6d0db4f5d4e3e
brunorwanda4 <brunorwanda4@gmail.com>
2025-01-27 11:45:04 +0200

    c

--- src/app/[lang]/(application)/class/[classId]/notes/page.tsx
+++ src/app/[lang]/(application)/class/[classId]/notes/page.tsx
@@ -6,12 +6,15 @@ const ClassNotesPage = () => {
   return (
     <div className=' p-4'>
       <SearchNotesClass />
+      <div className=' space-y-2 mt-3'>
+        <h3 className=' font-medium text-lg '>All notes</h3>
       <div className=' mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         <PostCard postRole='NOTES'/>
         <PostCard postRole='NOTES'/>
         <PostCard postRole='NOTES'/>
         <PostCard postRole='NOTES'/>
         <PostCard postRole='NOTES'/>
+      </div>
       </div>
       <div className=' h-screen'/>
     </div>
