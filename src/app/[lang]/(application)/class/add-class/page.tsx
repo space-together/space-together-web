f1976f5ebe1585f49afaaacad57b660977feb75e
brunorwanda4 <brunorwanda4@gmail.com>
2025-01-28 16:59:02 +0200

    make add class page

---
+++ src/app/[lang]/(application)/class/add-class/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateClassPage = () => {
+  return (
+    <div>
+      create class page
+    </div>
+  )
+}
+
+export default CreateClassPage
--- src/app/[lang]/(application)/teacher/page.tsx
+++ src/app/[lang]/(application)/teacher/page.tsx
@@ -1,9 +1,23 @@
+import { auth } from '@/auth';
+import { Locale } from '@/i18n';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const TeacherPage = () => {
+interface props {
+  params: Promise<{ lang: Locale }>;
+}
+
+const TeacherPage =async (props: props)  =>{
+  const params = await props.params;
+  const { lang } = params;
+  const user = (await auth())?.user;
+  if (!user) {
+    return redirect(`/${lang}/auth/login`);
+  }
+
   return (
     <div>
-      hello teacher
+      class add
     </div>
   )
 }
