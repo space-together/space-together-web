1ea3b6d2591430e16ee0df7921af7dd82ae8f1a2
brunorwanda4 <brunorwanda4@gmail.com>
2025-01-28 11:11:33 +0200

    make class layout

---
+++ src/app/[lang]/(application)/teacher/layout.tsx
@@ -0,0 +1,40 @@
+import { auth } from "@/auth";
+import { AppSidebar } from "@/components/site/navbar/app-sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { Locale } from "@/i18n";
+import { studentSidebarGroups } from "@/utils/context/app-side-content";
+import { redirect } from "next/navigation";
+
+interface props {
+  children: React.ReactNode;
+  params: Promise<{ lang: Locale }>;
+}
+
+export default async function TeacherLayout(props: props) {
+  const params = await props.params;
+  const { lang } = params;
+  const { children } = props;
+  const user = (await auth())?.user;
+  if (!user) {
+    return redirect(`/${lang}/auth/login`);
+  }
+
+  return (
+    <SidebarProvider className=" w-full">
+      <AppSidebar
+        user={{
+          ...user,
+          name: user.name ?? "",
+          email: user.email ?? undefined,
+          image: user.image ?? undefined,
+        }}
+        name="Student"
+        lang={lang}
+        items={studentSidebarGroups}
+      />
+      <main className="w-full">
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}
