83a54349211099984160117ba26c0faeb67f981b
brunorwanda4 <brunorwanda4@gmail.com>
2025-01-29 15:11:26 +0200

    make school staff

--- src/components/app/school/school-home-body.tsx
+++ src/components/app/school/school-home-body.tsx
@@ -3,19 +3,21 @@ import SchoolHomePosts from "./school-home-posts";
 import SchoolHomeAbout from "./school-home-about";
 import { Locale } from "@/i18n";
 import SchoolContacts from "./school-contacts";
+import SchoolStaff from "./school-staff";
 
 interface props {
-    lang : Locale
+  lang: Locale;
 }
 
-const SchoolHomeBody = ({lang} : props) => {
+const SchoolHomeBody = ({ lang }: props) => {
   return (
     <div className=" w-full space-y-4">
       <div className=" flex space-x-4 justify-between w-full">
-        <SchoolHomePosts lang={lang}/>
+        <SchoolHomePosts lang={lang} />
         <div className=" w-1/2 space-y-4">
           <SchoolHomeAbout />
           <SchoolContacts />
+          <SchoolStaff />
         </div>
       </div>
     </div>
---
+++ src/components/app/school/school-staff.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SchoolStaff = () => {
+  return (
+    <div className=" happy-card">
+      <div className="">
+        <h3 className=" font-semibold capitalize">school staff </h3>
+      </div>
+    </div>
+  );
+};
+
+export default SchoolStaff;
