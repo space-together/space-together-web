b6660abf334194c3186d5247185d611a30bdd21c
brunorwanda4 <brunorwanda4@gmail.com>
2025-01-30 17:04:38 +0200

    make school post page

--- src/app/[lang]/(application)/(other-users)/school/(main-school)/posts/page.tsx
+++ src/app/[lang]/(application)/(other-users)/school/(main-school)/posts/page.tsx
@@ -1,10 +1,24 @@
-
-const SchoolPostsPage = () => {
-  return (
-    <div  className=' h-screen'>
-      post page
-    </div>
-  )
+import { auth } from "@/auth";
+import SchoolHomePosts from "@/components/app/school/school-home-posts";
+import { Locale } from "@/i18n";
+import { redirect } from "next/navigation";
+interface props {
+  params: Promise<{ lang: Locale }>;
 }
 
-export default SchoolPostsPage
+const SchoolPostsPage = async (props: props) => {
+  const params = await props.params;
+  const { lang } = params;
+  const user = (await auth())?.user;
+  if (!user) {
+    return redirect(`/${lang}/auth/login`);
+  }
+  return (
+    <div className=" min-h-screen px-4 space-x-4 flex ">
+      <SchoolHomePosts className=" grid-cols-3" isOnSchoolPost lang={lang} />
+      <div className=" h-screen"/>
+    </div>
+  );
+};
+
+export default SchoolPostsPage;
--- src/components/app/school/school-home-posts.tsx
+++ src/components/app/school/school-home-posts.tsx
@@ -3,31 +3,38 @@ import CreateNewPostInSchool from "./create-new-post-in-school";
 import { FaSignsPost } from "react-icons/fa6";
 import PostCard from "@/components/cards/post-card";
 import { Locale } from "@/i18n";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 interface props {
   lang: Locale;
+  isOnSchoolPost?: boolean;
+  className?: string;
 }
 
-const SchoolHomePosts = ({ lang }: props) => {
+const SchoolHomePosts = ({ lang, isOnSchoolPost, className }: props) => {
   return (
     <div className="space-y-2 w-full">
-      <CreateNewPostInSchool />
-      <div>
-        <div className=" space-y-2">
-          <div className=" space-x-1 flex items-center">
-            <FaSignsPost />
-            <h2 className=" font-semibold">Posts</h2>
-          </div>
-          <div className=" grid grid-cols-1 w-full gap-4">
-            <PostCard lang={lang} postRole="IMAGE" />
-            <PostCard lang={lang} postRole="TEXT" />
-            <PostCard lang={lang} postRole="IMAGE" />
-            <PostCard lang={lang} postRole="TEXT" />
-          </div>
-          <div className=" happy-card justify-center items-center flex-row">
-            <span className=" link">See More</span>
-          </div>
+      {!isOnSchoolPost && <CreateNewPostInSchool />}
+      <div className=" space-y-2">
+        <div className=" space-x-1 flex items-center">
+          <FaSignsPost />
+          <h2 className=" font-semibold">Posts</h2>
         </div>
+        <div className={cn("grid grid-cols-1 w-full gap-4", className)}>
+          <PostCard lang={lang} postRole="IMAGE" />
+          <PostCard lang={lang} postRole="TEXT" />
+          <PostCard lang={lang} postRole="IMAGE" />
+          <PostCard lang={lang} postRole="TEXT" />
+        </div>
+        {!isOnSchoolPost && (
+          <Link
+            href={`/${lang}/school/posts`}
+            className=" happy-card justify-center items-center flex-row"
+          >
+            <span className=" link">See More</span>
+          </Link>
+        )}
       </div>
     </div>
   );
