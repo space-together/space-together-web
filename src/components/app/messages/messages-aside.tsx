991921b248428c31ce4df05d5ff1b08d5c807f53
brunorwanda4 <brunorwanda4@gmail.com>
2025-01-31 06:39:41 +0200

    make messages seacher

--- src/components/app/messages/messages-aside.tsx
+++ src/components/app/messages/messages-aside.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import MessagesAsideNavbar from "./message-aside-navbar";
+import MessageSearch from "./messages-search";
 
 const MessagesAside = () => {
   return (
-    <aside className=" fixed w-80 h-screen bg-base-100">
+    <aside className=" fixed w-80 h-screen bg-base-100 flex">
       <MessagesAsideNavbar />
-      <div className=" w-full border-r border-r-border">hello messages</div>
+      <div className="w-full max-h-screen min-h-screen border-r border-r-border">
+        <MessageSearch />
+      </div>
     </aside>
   );
 };
