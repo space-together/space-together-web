facde55582a8cb26e6e2f68faae792c43848db36
brunorwanda4 <brunorwanda4@gmail.com>
2025-01-31 12:11:48 +0200

    finish to make conversation page

--- src/components/app/messages/conversation-navbar.tsx
+++ src/components/app/messages/conversation-navbar.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import React from "react";
 
 const ConversationNavbar = () => {
   return (
-    <nav className=' h-12 bg-base-100 border-b border-b-border px-4'>
-      hello
+    <nav className=" h-12 bg-base-100 border-b border-b-border px-4 flex items-center">
+      <div className=" flex space-x-2">
+        <Avatar className=" size-10">
+          <AvatarImage src="/images/2.jpg" />
+          <AvatarFallback>PR</AvatarFallback>
+        </Avatar>
+        <div className=" flex flex-col">
+          <h3 className=" font-medium"> Bruno Happy heart</h3>
+          <span className=" font-medium text-myGray text-sm">STUDENT</span>
+        </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default ConversationNavbar
+export default ConversationNavbar;
