17d287f6034cd8a9338e504c57d2da99fbb13126
brunorwanda4 <brunorwanda4@gmail.com>
2025-02-01 20:18:27 +0200

    make user tooltip

--- src/app/[lang]/(application)/(other-users)/class/page.tsx
+++ src/app/[lang]/(application)/(other-users)/class/page.tsx
@@ -1,13 +1,34 @@
 import ClassTimetable from "@/components/app/class/classTimetable";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import UserTooltip from "@/context/tooltip/user-tooltip";
+import { Locale } from "@/i18n";
 import { getSectorsByEducationId } from "@/services/data/sector-data";
 
-const ClassIdPage = async () => {
-  const educationClass = await getSectorsByEducationId("6797b81f071fbeb2d8b5512c");
+interface Props {
+  params: Promise<{ lang: Locale }>;
+}
+
+const ClassIdPage = async (props: Props) => {
+  const params = await props.params;
+  const { lang } = params;
+  const educationClass = await getSectorsByEducationId(
+    "6797b81f071fbeb2d8b5512c"
+  );
   return (
     <div className=" p-4 min-h-screen h-full ">
       <ClassTimetable />
-      {educationClass ? educationClass.map((item) => (<div key={item.id}>{item.name}</div>)) : "No education for sector"}
-
+      {educationClass
+        ? educationClass.map((item) => <div key={item.id}>{item.name}</div>)
+        : "No education for sector"}
+      <UserTooltip
+        lang={lang}
+        trigger={
+          <Avatar className=" size-12">
+            <AvatarImage src="/images/2.jpg" />
+            <AvatarFallback>PR</AvatarFallback>
+          </Avatar>
+        }
+      />
     </div>
   );
 };
---
+++ src/context/tooltip/user-tooltip.tsx
@@ -0,0 +1,53 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { Locale } from "@/i18n";
+import { Dot } from "lucide-react";
+import Link from "next/link";
+
+interface props {
+  trigger: string | React.ReactNode;
+  lang: Locale;
+}
+
+export default function UserTooltip({ trigger , lang}: props) {
+  return (
+    <HoverCard open>
+      <HoverCardTrigger asChild>{trigger}</HoverCardTrigger>
+      <HoverCardContent className=" w-72">
+        <div className="space-y-3">
+          <div className="flex items-center gap-3">
+            <Avatar className=" size-12">
+              <AvatarImage src="/images/2.jpg" />
+              <AvatarFallback>PR</AvatarFallback>
+            </Avatar>
+            <div>
+              <div className="space-y-0.5 flex space-x-2">
+                <h5 className="text-sm font-medium">Keith Kennedy</h5>
+                <p className="text-xs text-muted-foreground">@k.kennedy</p>
+              </div>
+              <strong className=" text-sm text-myGray">STUDENT</strong>
+            </div>
+          </div>
+          <div className=" flex space-x-2">
+            <div className=" flex -space-x-2 text-myGray items-center text-sm">
+              <Dot size={32} />
+              <span>SOSTHCS</span>
+            </div>
+            <div className=" flex -space-x-2 text-myGray items-center text-sm">
+              <Dot size={32} />
+              <span>L5SOD</span>
+            </div>
+          </div>
+          <Link href={`/${lang}/messages/student`}>
+          <Button variant="info" size="sm" className=" w-full">Message</Button>
+          </Link>
+        </div>
+      </HoverCardContent>
+    </HoverCard>
+  );
+}
