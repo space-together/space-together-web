ae702eb64bee06ec33b8700bb2cc465453ddaf96
brunorwanda4 <brunorwanda4@gmail.com>
2025-02-01 09:10:13 +0200

    add link on setting

--- src/components/app/messages/message-aside-left-navbar.tsx
+++ src/components/app/messages/message-aside-left-navbar.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 import { Locale } from "@/i18n";
 import Link from "next/link";
 import React from "react";
-import { AiOutlineSetting } from "react-icons/ai";
 interface props {
   lang: Locale;
 }
@@ -11,16 +10,20 @@ interface props {
 const MessagesAsideLeftNavbar = ({ lang }: props) => {
   return (
     <div className=" w-16 h-screen border-r border-r-border p-2 space-y-4">
-      <Link href={`/${lang}/school`}>
-        <Button shape="square">
-          <MyImage src="/icons/school.png" className=" size-8"/>
-        </Button>
-      </Link>
       <Button shape="square">
-      <MyImage src="/icons/blackboard.png" className=" size-8"/>
+        <Link href={`/${lang}/school`}>
+          <MyImage src="/icons/school.png" className=" size-8" />
+        </Link>
       </Button>
       <Button shape="square">
-        <AiOutlineSetting size={24} />
+        <Link href={`/${lang}/class`}>
+          <MyImage src="/icons/blackboard.png" className=" size-8" />
+        </Link>
+      </Button>
+      <Button shape="square">
+        <Link href={`/${lang}/settings`}>
+          <MyImage src="/icons/cogwheel.png" className=" size-8" />
+        </Link>
       </Button>
     </div>
   );
