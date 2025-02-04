5894d2c4e697e2940b1b27369f478f9938908fb0
brunorwanda4 <brunorwanda4@gmail.com>
2025-02-04 19:09:04 +0200

    fix error  student page

--- src/app/[lang]/(application)/(other-users)/student/page.tsx
+++ src/app/[lang]/(application)/(other-users)/student/page.tsx
@@ -1,16 +1,31 @@
+import { auth } from "@/auth";
 import ClassActivities from "@/components/app/class/classActivities";
 import ClassBody from "@/components/app/class/classBody";
 import ClassHead from "@/components/app/class/classHead";
 import { Separator } from "@/components/ui/separator";
+import { Locale } from "@/i18n";
+import { redirect } from "next/navigation";
 
-const StudentPage = () => {
+
+interface props {
+  params: Promise<{ lang: Locale }>;
+}
+
+
+const StudentPage = async (props: props) => {
+  const params = await props.params;
+  const { lang } = params;
+  const user = (await auth())?.user;
+  if (!user) {
+    return redirect(`/${lang}/auth/login`);
+  }
   return (
     <div className=" px-4">
-      <ClassHead />
+      <ClassHead lang={lang}/>
       <div className=" mt-28">
         <Separator />
         <div className="flex  justify-between space-x-4 mt-4">
-          <ClassBody />
+          <ClassBody lang={lang}/>
           <ClassActivities />
         </div>
       </div>
