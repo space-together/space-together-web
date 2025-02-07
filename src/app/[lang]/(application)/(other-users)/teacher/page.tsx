1056e2ce02885b23ad9bb0681230145825096379
brunorwanda4 <brunorwanda4@gmail.com>
2025-02-07 17:37:45 +0200

    get class

--- src/app/[lang]/(application)/(other-users)/teacher/page.tsx
+++ src/app/[lang]/(application)/(other-users)/teacher/page.tsx
@@ -2,7 +2,7 @@ import { auth } from "@/auth";
 import CreateClassDialog from "@/components/app/class/createClassDialog";
 import TeacherHomeBody from "@/components/app/teacher/teacher-home-body";
 import { Locale } from "@/i18n";
-import { getAllClasses } from "@/services/data/class-data";
+import { getAllClassesByUserId } from "@/services/data/class-data";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -14,11 +14,11 @@ const TeacherPage = async (props: props) => {
   const params = await props.params;
   const { lang } = params;
   const user = (await auth())?.user;
-  if (!user) {
+  if (!user?.id) {
     return redirect(`/${lang}/auth/login`);
   }
 
-  const classes= await getAllClasses();
+  const classes = await getAllClassesByUserId(user.id);
   return (
     <div className=" px-4">
       {!classes ? (
@@ -26,7 +26,7 @@ const TeacherPage = async (props: props) => {
           <CreateClassDialog />
         </div>
       ) : (
-        <TeacherHomeBody classes={classes} lang={lang}/>
+        <TeacherHomeBody classes={classes} lang={lang} />
       )}
     </div>
   );
