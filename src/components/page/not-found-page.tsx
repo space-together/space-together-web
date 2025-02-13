a564164feb90e0a63598b85599a56bd972c8e9f1
brunorwanda4 <brunorwanda4@gmail.com>
2025-02-13 11:48:03 +0200

    make not found page

--- src/components/page/not-found-page.tsx
+++ src/components/page/not-found-page.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+"use client";
+import React from "react";
+import MyImage from "../my-components/myImage";
+import { Button } from "../ui/button";
+import { RefreshCcw } from "lucide-react";
+import { BsArrowLeft } from "react-icons/bs";
+import { useRouter } from "next/navigation";
 
 const NotFoundPage = () => {
-  return (
-    <div>
-      not found page
-    </div>
-  )
-}
+  const router = useRouter();
 
-export default NotFoundPage
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
+  return (
+    <div className="flex w-full justify-center">
+      <div className=" flex flex-col">
+        <MyImage className="size-96" src="/notFound.svg" />
+        <div>
+          <div className="text-center">
+          <h4 className="  font-medium"> This collection is not found!</h4>
+           <p className=" text-myGray">Check your internet connection or your params for this page.</p>
+          </div>
+          <div className="flex space-x-2 mt-2 justify-center">
+            <Button size="sm" variant="outline" onClick={handleGoBack}>
+              <BsArrowLeft /> Go back
+            </Button>
+            <Button size="sm" variant="info" onClick={handleRefresh}>
+              Refresh <RefreshCcw size={12} />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
