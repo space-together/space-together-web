38a7afc787e3fbbe046c99358eb5fcf709afc55a
brunorwanda4 <brunorwanda4@gmail.com>
2025-02-14 10:56:51 +0200

    c

--- src/components/site/navbar/site-logo.tsx
+++ src/components/site/navbar/site-logo.tsx
@@ -8,28 +8,16 @@ import Link from "next/link";
 import React from "react";
 
 interface props {
-  user ?: authUser,
-  lang : Locale,
+  user?: authUser;
+  lang: Locale;
 }
 
-const SiteLogo = ({user, lang} : props) => {
-  if (user)
-    return (
-      <Link href={`${user  ? RedirectContents({lang , role : user.role}) : "/"}`} className=" flex gap-2 items-center">
-        <MyImage src="/logo/1.png" className=" size-10" />
-        <div className=" flex flex-col">
-          <h2
-            className={cn(
-              "font-semibold text-base text-start font-allura flex flex-row"
-            )}
-          >
-            space-together
-          </h2>
-        </div>
-      </Link>
-    );
+const SiteLogo = ({ user, lang }: props) => {
   return (
-    <div className=" flex gap-2 items-center">
+    <Link
+      href={`${user ? RedirectContents({ lang, role: user.role }) : "/"}`}
+      className=" flex gap-2 items-center"
+    >
       <MyImage src="/logo/1.png" className=" size-10" />
       <div className=" flex flex-col">
         <h2
@@ -40,7 +28,7 @@ const SiteLogo = ({user, lang} : props) => {
           space-together
         </h2>
       </div>
-    </div>
+    </Link>
   );
 };
 
