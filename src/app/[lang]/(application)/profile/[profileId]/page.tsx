928b676e0dd32485605660abd0e1d53c9397a94e
brunorwanda4 <brunorwanda4@gmail.com>
2025-02-24 11:38:34 +0200

    c

--- src/app/[lang]/(application)/profile/[profileId]/page.tsx
+++ src/app/[lang]/(application)/profile/[profileId]/page.tsx
@@ -1,17 +1,28 @@
+import { auth } from '@/auth';
 import ProfileBodyOtherUser from '@/components/app/profile/profile-body-other-user';
 import ProfileHeader from '@/components/app/profile/profile-header'
 import { Locale } from '@/i18n';
-import React from 'react'
-interface Props {
+import { redirect } from 'next/navigation';
+
+interface props {
+  children: React.ReactNode;
   params: Promise<{ lang: Locale }>;
 }
-
-const ProfileIdPage =  async (props: Props) => {
+const ProfileIdPage = async (props: props) => {
   const params = await props.params;
   const { lang } = params;
+  const currentUser = (await auth())?.user;
+  if (!currentUser || !currentUser.id) {
+    return redirect(`/${lang}/auth/login`);
+  }
   return (
     <div className=" p-4 space-y-4 w-full">
-      <ProfileHeader OtherUser lang={lang}/>
+      <ProfileHeader   user={{
+          ...currentUser,
+          name: currentUser.name ?? "",
+          email: currentUser.email ?? undefined,
+          image: currentUser.image ?? undefined,
+        }} OtherUser lang={lang}/>
       <ProfileBodyOtherUser lang={lang}/>
     </div>
   )
