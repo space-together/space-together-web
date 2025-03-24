699863923e8eb60ed228fa12b5101e4208b040f5
brunorwanda4 <brunorwanda4@gmail.com>
2025-03-24 04:11:04 +0200

    make simple session

--- src/services/auth/core/base.tsx
+++ src/services/auth/core/base.tsx
@@ -1,13 +1,22 @@
 import apiRequest from "@/services/api-request";
 import { registerSchema, registerSchemaType } from "@/utils/schema/userSchema";
+import z from "zod";
+
+export const userSession = z.object({
+  id: z.string(),
+  user_id : z.string(),
+  token: z.string(),
+  session_token: z.string(),
+  created_at: z.string(),
+  expires_at: z.string(),
+  updated_at: z.string(),
+})
+export type userSessionType = z.infer<typeof userSession>
 
 export const registerAuth = (data :registerSchemaType ) => {
  const validation = registerSchema.safeParse(data);
   if (!validation.success) {
     return { error: "Invalid Register Validation" };
   }
-
-//   const { name, email, password } = validation.data;
-
-  apiRequest<registerSchemaType, string>('post', '/auth/register',data);
+  apiRequest<registerSchemaType, userSessionType>('post', '/auth/register',data);
 }
