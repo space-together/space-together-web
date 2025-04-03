a07453beb119605af4c549a9e20c77152712b8ed
brunorwanda4 <brunorwanda4@gmail.com>
2025-04-03 23:42:20 +0200

    c

--- src/app/[lang]/(application)/school-staff/create-school/page.tsx
+++ src/app/[lang]/(application)/school-staff/create-school/page.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import PermissionPage from "@/components/page/permission-page";
+import { Locale } from "@/i18n";
+import { getCurrentUser } from "@/services/auth/core/current-user";
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
 
-const SchoolStaffCreateSchoolPage = () => {
-  return (
-    <div>
-      create school page
-    </div>
-  )
+export const metadata: Metadata = {
+  title: "Create - school",
+};
+
+interface props {
+  params: Promise<{ lang: Locale }>;
 }
 
-export default SchoolStaffCreateSchoolPage
+const SchoolStaffCreateSchoolPage = async (props: props) => {
+  const params = await props.params;
+  const { lang } = params;
+  const currentUser = await getCurrentUser({ withFullUser: true });
+  if (!currentUser) return redirect(`/${lang}/auth/login`);
+  if (currentUser.role !== "SCHOOLSTAFF") return <PermissionPage />;
+  return <div> <PermissionPage /></div>;
+};
+
+export default SchoolStaffCreateSchoolPage;
--- src/components/page/permission-page.tsx
+++ src/components/page/permission-page.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+"use client";
+import MyImage from "../my-components/myImage";
+import { Button } from "../ui/button";
+import { RefreshCcw } from "lucide-react";
+import { BsArrowLeft } from "react-icons/bs";
+import { useRouter } from "next/navigation";
 
-const PermissionPage = () => {
-  return (
-    <div>
-      permission page
-    </div>
-  )
+interface props {
+  description?: string;
+}
+
+
+const PermissionPage = ({description} : props) => {
+ const router = useRouter();
+     
+       const handleRefresh = () => {
+         window.location.reload();
+       };
+     
+       const handleGoBack = () => {
+         router.back();
+       };
+   return (
+     <div className="flex w-full justify-center">
+       <div className=" flex flex-col">
+         <MyImage
+           className=" h-80 w-96"
+           classname=" object-contain"
+           src="/png/permission.png"
+         />
+         <div>
+           <div className="text-center">
+             <p className=" ">{description}</p>
+           </div>
+           <div className="flex space-x-2 mt-2 justify-center">
+             <Button size="sm" variant="outline" onClick={handleGoBack}>
+               <BsArrowLeft /> Go back
+             </Button>
+             <Button size="sm" variant="info" onClick={handleRefresh}>
+               Refresh <RefreshCcw size={12} />
+             </Button>
+           </div>
+         </div>
+       </div>
+     </div>
+   );
 }
 
 export default PermissionPage
